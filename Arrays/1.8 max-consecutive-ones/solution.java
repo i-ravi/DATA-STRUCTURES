945e07a6e028e6abccd24fad48dd337d0feb93d0
Ravi Sharma <49070597+ravi-sharma-SDE@users.noreply.github.com>
2024-05-14 23:26:21 +0530

    Added solution

---
+++ Arrays/1.8 max-consecutive-ones/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == 1){
+                currentCount++;
+            } else {
+                if (currentCount > maxCount){
+                    maxCount = currentCount;
+                }
+                currentCount = 0;
+            }
+        }
+        if (currentCount > maxCount){
+            maxCount = currentCount;
+        }
+
+        return maxCount;
+    }
+}
